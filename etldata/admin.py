b033d8ef6e523d21be5d4e76d917605f997e4a05
nathanhilbert <nathanhilbert@gmail.com>
2014-10-23 11:35:37 -0400

    adding permissions

--- etldata/admin.py
+++ etldata/admin.py
@@ -6,15 +6,46 @@ import reversion
 from etldata.models import DataConnection, MetaData
 
 
-class DataConnectionAdmin(admin.ModelAdmin):
+class DataConnectionAdmin(reversion.VersionAdmin):
     list_display = ('indicator','sectorid','organization','status',)
     #fieldsets =[(None,{'fields': ['categoryName']}),]
 
-    def get_form(self, request, obj=None, **kwargs):
-        if not request.user.is_superuser:
-            self.exclude.append('preprocessors')
-            self.exclude.append('raw_response')
-        return super(DataConnectionAdmin, self).get_form(request, obj, **kwargs)
+    # def get_form(self, request, obj=None, **kwargs):
+    #     if not request.user.is_superuser:
+    #         self.exclude.append('preprocessors')
+    #         self.exclude.append('raw_response')
+    #     return super(DataConnectionAdmin, self).get_form(request, obj, **kwargs)
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.readonly_fields
+
+        if self.declared_fieldsets:
+            return flatten_fieldsets(self.declared_fieldsets)
+        else:
+            return list(set(
+                [field.name for field in self.opts.local_fields] +
+                [field.name for field in self.opts.local_many_to_many]
+            ))
+
+
+    # def __init__(self, *args, **kwargs):
+    #     super(DataConnectionAdmin, self).__init__(*args, **kwargs)
+    #     self.list_display_links = (None,)
+
+
+    # def has_add_permission(self, request, obj=None):
+    #     if request.user.groups.filter(name='superadmin').exists() or request.user.groups.filter(name='editor').exists():
+    #         return True
+    #     else:
+    #         return False
+    #
+    # def has_delete_permission(self, request, obj=None):
+    #     if request.user.groups.filter(name='superadmin').exists() or request.user.groups.filter(name='editor').exists():
+    #         return True
+    #     else:
+    #         return False
+
 
 
 
@@ -23,6 +54,27 @@ class MetaDataAdmin(reversion.VersionAdmin):
     #fieldsets =[(None,{'fields': ['theme']}),]
 
 
+    def has_add_permission(self, request, obj=None):
+        if request.user.groups.filter(name='superadmin').exists() or request.user.groups.filter(name='editor').exists():
+            return True
+        else:
+            return False
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.groups.filter(name='superadmin').exists() or request.user.groups.filter(name='editor').exists():
+            return True
+        else:
+            return False
+
+
+    # def __init__(self, *args, **kwargs):
+    #     super(MetaDataAdmin, self).__init__(*args, **kwargs)
+    #     self.list_display_links = (None,)
+
+
+
 
 admin.site.register(DataConnection, DataConnectionAdmin)
 admin.site.register(MetaData, MetaDataAdmin)
+
+
